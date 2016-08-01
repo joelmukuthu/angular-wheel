456aa430adab368092e9a839767069ea26dc5bc5
Joel Mukuthu <joelmukuthu@gmail.com>
2016-08-02 01:33:09 +0200

    Fix path to packaged sinon test dependency

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
         files: [
             'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
-            'node_modules/sinon/pkg/sinon-1.17.4.js',
+            'node_modules/sinon/pkg/sinon.js',
             'node_modules/unexpected/unexpected.js',
             'node_modules/unexpected-sinon/lib/unexpected-sinon.js',
             'src/*.js',
