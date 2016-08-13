e173451771efee386d505e2ef3b1b1005551cd30
Joel Mukuthu <joelmukuthu@gmail.com>
2016-08-13 17:06:12 +0200

    Fix licence year

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,8 @@ module.exports = function (grunt) {
 
         info: {
             banner: {
-                short: '/* <%= pkg.name %> v<%= pkg.version %>, (c) 2014-<%= grunt.template.today("yyyy") %> Joel Mukuthu, MIT License, built: <%= grunt.template.date("dd-mm-yyyy HH:MM:ss Z") %> */\n',
-                long: '/**\n * <%= pkg.name %>\n * Version: <%= pkg.version %>\n * (c) 2014-<%= grunt.template.today("yyyy") %> Joel Mukuthu\n * MIT License\n * Built on: <%= grunt.template.date("dd-mm-yyyy HH:MM:ss Z") %>\n **/\n\n'
+                short: '/* <%= pkg.name %> v<%= pkg.version %>, (c) <%= grunt.template.today("yyyy") %> Joel Mukuthu, MIT License, built: <%= grunt.template.date("dd-mm-yyyy HH:MM:ss Z") %> */\n',
+                long: '/**\n * <%= pkg.name %>\n * Version: <%= pkg.version %>\n * (c) <%= grunt.template.today("yyyy") %> Joel Mukuthu\n * MIT License\n * Built on: <%= grunt.template.date("dd-mm-yyyy HH:MM:ss Z") %>\n **/\n\n'
             }
         },
 
